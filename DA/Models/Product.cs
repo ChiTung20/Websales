3228ef94eb0307aba7d256c17abf905aa1526960
ChiTung20 <nchitung20sn@gmail.com>
2023-03-08 14:25:26 +0700

    add 2

--- DA/Models/Product.cs
+++ DA/Models/Product.cs
@@ -12,7 +12,7 @@ namespace DA.Models
 {
     public partial class Product
     {
-        //public Product()
+        //public Product() ()()))))))))))))))))))))))))))))))
         //{
         //    Thumb = "~/adminassets/img/products/avater.jpg";
         //}
